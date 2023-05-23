99af7dc5229d51edf06b91645d335b4a2480c48e
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-05-23 15:11:55 +0600

    finished

--- app/src/main/java/com/example/texttospeech/MainActivity.java
+++ app/src/main/java/com/example/texttospeech/MainActivity.java
@@ -3,12 +3,51 @@ package com.example.texttospeech;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button convertBtn;
+    EditText editText;
+    TextToSpeech textToSpeech;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        convertBtn=findViewById(R.id.convertBtn);
+        editText=findViewById(R.id.editText);
+
+        textToSpeech=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status!=TextToSpeech.ERROR){
+                    textToSpeech.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+        convertBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getText=editText.getText().toString();
+                textToSpeech.speak(getText,TextToSpeech.QUEUE_FLUSH,null);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onPause() {
+        if (textToSpeech!=null){
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onPause();
     }
 }
